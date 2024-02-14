5119cb8cf9d828227c4ab514e78cd13492dbda29
Scott Wright <scott.wright.uk@gmail.com>
2024-02-14 14:11:38 +0000

    feat: add initial Header component

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,17 @@
-import { useState } from 'react';
+import Header from './components/Header';
+import SectionContainer from './components/SectionContainer';
+import Footer from './components/Footer';
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <div className="mx-auto max-w-7xl px-4 pt-4 sm:px-6 lg:px-8">
+        <Header />
+        <SectionContainer />
+        <Footer />
+      </div>
+    </>
+  );
 }
 
 export default App;
