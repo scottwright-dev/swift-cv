a802925830b523129b814a2ede605c6bfc146269
Scott Wright <scott.wright.uk@gmail.com>
2024-02-15 12:29:43 +0000

    feat: add ButtonPrimary component

---
+++ src/components/ButtonPrimary.jsx
@@ -0,0 +1,18 @@
+import PropTypes from 'prop-types';
+
+function ButtonPrimary({ text }) {
+  return (
+    <button
+      type="button"
+      className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+    >
+      {text}
+    </button>
+  );
+}
+
+ButtonPrimary.propTypes = {
+  text: PropTypes.string.isRequired,
+};
+
+export default ButtonPrimary;
