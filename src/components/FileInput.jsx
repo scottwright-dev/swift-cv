b92439ad76c781958eb32a0b619f773996fbe7fe
Scott Wright <scott.wright.uk@gmail.com>
2024-02-17 12:27:02 +0000

    feat: Add FileInput component
    A summary of the FileInput component functionality:
    
    - This component replaces the native file input and integrates the TailwindUI styling for a more consistent UX.
    - `useRef` is used for DOM manipulation, this is needed to allow for the opening of the file dialog when the custom button is clicked.
    - `ButtonPrimary` component is used as a trigger for the file input, and maintains design consistency across the app.
    - Handles file selection with a callback function, enabling the parent component to access the selected file immediately.
    - Added `aria-label` with label and optional flag for a11y.

---
+++ src/components/FileInput.jsx
@@ -0,0 +1,59 @@
+import { useRef } from 'react';
+import PropTypes from 'prop-types';
+import ButtonPrimary from './ButtonPrimary';
+
+function FileInput({ label, isOptional, onFileSelect }) {
+  // ref to store file input DOM element for direct manipulation
+  const fileInputRef = useRef(null);
+
+  // function to handle file selection change event
+  const handleFileChange = (event) => {
+    // check if any file is selected and invoke the callback with the file
+    if (event.target.files.length > 0) {
+      onFileSelect(event.target.files[0]);
+    }
+  };
+
+  // function to click the hidden file input when the custom button is clicked
+  const handleClick = () => {
+    fileInputRef.current.click();
+  };
+
+  return (
+    <div className="mb-4 flex items-center space-x-6">
+      <div className="shrink-0">
+        <span className="inline-block h-14 w-14 overflow-hidden rounded-full bg-slate-200">
+          <svg
+            className="h-full w-full text-indigo-300"
+            fill="currentColor"
+            viewBox="0 0 24 24"
+          >
+            <path d="M24 20.993V24H0v-2.996A14.977 14.977 0 0112.004 15c4.904 0 9.26 2.354 11.996 5.993zM16.002 8.999a4 4 0 11-8 0 4 4 0 018 0z" />
+          </svg>
+        </span>
+      </div>
+      <div>
+        <label className="block text-sm font-medium leading-6 text-gray-900">
+          {label}
+          {isOptional && ' (optional)'}
+        </label>
+        <input
+          ref={fileInputRef}
+          type="file"
+          onChange={handleFileChange}
+          className="hidden"
+          aria-label={label + (isOptional ? ' (optional)' : '')}
+        />
+        <ButtonPrimary text="Add Photo" onClick={handleClick} />
+      </div>
+    </div>
+  );
+}
+
+FileInput.propTypes = {
+  label: PropTypes.string.isRequired,
+  isOptional: PropTypes.bool,
+  onFileSelect: PropTypes.func.isRequired,
+};
+
+export default FileInput;
