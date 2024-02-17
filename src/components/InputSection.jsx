c05436d9b9805afc66d75f6a4953408065a74b67
Scott Wright <scott.wright.uk@gmail.com>
2024-02-17 12:38:27 +0000

    feat: Integrate FileInput component into form
    - Added a 'Photo' field with 'file' type to the 'Personal Information' section, to users to upload images.
    - Integrated `FileInput` component to handle file uploads.
    - Used conditional rendering within form to distinguish between standard input fields and a file input.

--- src/components/InputSection.jsx
+++ src/components/InputSection.jsx
@@ -1,28 +1,20 @@
 import { Disclosure } from '@headlessui/react';
 import { ChevronRightIcon } from '@heroicons/react/24/outline';
 import InputBasic from './InputBasic';
+import FileInput from './FileInput';
 import ButtonCircular from './ButtonCircular';
 
 const formSections = [
   {
     title: 'Personal Information',
     fields: [
+      { label: 'Photo', type: 'file', placeholder: '', isOptional: true },
       { label: 'Name', type: 'text', placeholder: '' },
       { label: 'Surname', type: 'text', placeholder: '' },
       { label: 'Phone', type: 'text', placeholder: '' },
       { label: 'Email', type: 'email', placeholder: '' },
-      {
-        label: 'LinkedIn',
-        type: 'url',
-        placeholder: '',
-        isOptional: true,
-      },
-      {
-        label: 'Website',
-        type: 'url',
-        placeholder: '',
-        isOptional: true,
-      },
+      { label: 'LinkedIn', type: 'url', placeholder: '', isOptional: true },
+      { label: 'Website', type: 'url', placeholder: '', isOptional: true },
     ],
   },
   {
@@ -92,22 +84,31 @@ export default function InputSection() {
               >
                 {({ open }) => (
                   <>
-                    <Disclosure.Button className="flex w-full justify-between rounded-lg bg-slate-100 px-4 py-2 text-left text-sm font-medium text-slate-900 hover:bg-slate-200 focus:outline-none focus-visible:ring focus-visible:ring-purple-500 focus-visible:ring-opacity-75">
+                    <Disclosure.Button className="flex w-full justify-between rounded-lg bg-slate-200 px-4 py-2 text-left text-sm font-medium text-slate-900 hover:bg-slate-200 focus:outline-none focus-visible:ring focus-visible:ring-purple-500 focus-visible:ring-opacity-75">
                       <span>{section.title}</span>
                       <ChevronRightIcon
                         className={`${open ? 'rotate-90 transform' : ''} h-5 w-5 text-slate-500`}
                       />
                     </Disclosure.Button>
                     <Disclosure.Panel className="px-4 pb-2 pt-4 text-sm text-gray-500">
-                      {section.fields.map((field, fieldIdx) => (
-                        <InputBasic
-                          key={`${sectionIdx}-${fieldIdx}`}
-                          label={field.label}
-                          type={field.type}
-                          placeholder={field.placeholder}
-                          isOptional={field.isOptional}
-                        />
-                      ))}
+                      {section.fields.map((field, fieldIdx) => {
+                        return field.type === 'file' ? (
+                          <FileInput
+                            key={`${sectionIdx}-${fieldIdx}`}
+                            label={field.label}
+                            isOptional={field.isOptional}
+                            onFileSelect={(file) => console.log(file)}
+                          />
+                        ) : (
+                          <InputBasic
+                            key={`${sectionIdx}-${fieldIdx}`}
+                            label={field.label}
+                            type={field.type}
+                            placeholder={field.placeholder}
+                            isOptional={field.isOptional}
+                          />
+                        );
+                      })}
                       {section.addButton && (
                         <div className="mt-4 flex justify-center">
                           <ButtonCircular text="Add More" />
