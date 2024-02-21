e317cd35da99363014c369d7daf179178405a4c7
Scott Wright <scott.wright.uk@gmail.com>
2024-02-21 15:54:48 +0000

    refactor: remove label text from output

--- src/components/OutputSection.jsx
+++ src/components/OutputSection.jsx
@@ -1,13 +1,11 @@
 import PropTypes from 'prop-types';
-
 function OutputSection({ formValues = {} }) {
   return (
     <div className="mx-auto max-w-7xl flex-1 px-4 pt-14 shadow-2xl sm:px-6 lg:px-8">
       <ul>
-        {Object.entries(formValues).map(([key, value]) => (
-          <li key={key} className="mb-2">
-            <span className="font-bold">{key.replace(/_/g, ' ')}:</span>
-            <span className="ml-2">{value}</span>
+        {Object.values(formValues).map((value, index) => (
+          <li key={index} className="mb-2">
+            <span>{value}</span>
           </li>
         ))}
       </ul>
