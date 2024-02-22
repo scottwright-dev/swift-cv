ec6d6ba224a1d12792df5f9d46043e8a1bd0bec5
Scott Wright <scott.wright.uk@gmail.com>
2024-02-22 11:27:04 +0000

    refactor: OutputSection component
    This refactor introduces the following changes:
    
    - Added unique IDs for key assignment.
    - Introduces isImage function to render content based on type, either image or text.
    - Minor tailwind style changes for simplicity.

--- src/components/OutputSection.jsx
+++ src/components/OutputSection.jsx
@@ -1,17 +1,24 @@
 import PropTypes from 'prop-types';
 
 function OutputSection({ formValues = {} }) {
+  const isImage = (value) =>
+    typeof value === 'string' && value.startsWith('blob:');
   return (
     <div className="mx-auto max-w-7xl flex-1 px-4 pt-14 shadow-2xl sm:px-6 lg:px-8">
-      {Object.entries(formValues).map(([key, value]) => {
-        const isImage = typeof value === 'string' && value.startsWith('blob:');
+      {Object.entries(formValues).map(([id, value]) => {
         return (
-          <div key={key} className="mb-2">
-            {isImage ? (
-              <img className="w-40" src={value} alt="Profile photo" />
-            ) : (
-              <span>{value}</span>
-            )}
+          <div key={id} className="mb-2">
+            <div className="ml-4">
+              {isImage(value) ? (
+                <img
+                  className="h-24 w-24 rounded-full object-cover"
+                  src={value}
+                  alt={`Profile photo`}
+                />
+              ) : (
+                <span>{value}</span>
+              )}
+            </div>
           </div>
         );
       })}
