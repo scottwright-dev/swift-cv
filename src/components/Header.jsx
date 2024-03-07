0240facf257a29b3d124d0ed5d156af50631f52c
Scott Wright <108872957+scottwright-dev@users.noreply.github.com>
2024-03-07 09:06:00 +0000

    refactor: Add semantic tags Header component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,7 +7,7 @@ export default function Header() {
   };
 
   return (
-    <div className="md:flex md:items-center md:justify-between">
+    <header className="md:flex md:items-center md:justify-between">
       <div className="min-w-0 flex-1">
         <h2 className="text-2xl font-bold leading-7 text-gray-900 sm:truncate sm:text-3xl sm:tracking-tight">
           CV Builder
@@ -16,6 +16,6 @@ export default function Header() {
       <div className="mt-4 flex md:ml-4 md:mt-0">
         <ButtonPrimary text="Create PDF" onClick={onClick} />
       </div>
-    </div>
+    </header>
   );
 }
