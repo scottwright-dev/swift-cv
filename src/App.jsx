c27379c226b92fe11bfe63657fb8095fc7ce506a
Scott Wright <108872957+scottwright-dev@users.noreply.github.com>
2024-03-28 12:18:16 +0000

    refactor: Move header comp to output section & rename footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,12 @@
-import Header from './components/Header';
 import SectionContainer from './components/SectionContainer';
-import Footer from './components/Footer';
+import AppFooter from './components/AppFooter';
 
 function App() {
   return (
     <>
       <div className="mx-auto max-w-7xl px-4 pt-4 sm:px-6 lg:px-8">
-        <Header />
         <SectionContainer />
-        <Footer />
+        <AppFooter />
       </div>
     </>
   );
