f8dc3cd105d4d9fec74bb1343907f8e488aceca2
Scott Wright <108872957+scottwright-dev@users.noreply.github.com>
2024-03-29 11:28:12 +0000

    feat: change app name to Swift CV

--- src/components/AppHeader.jsx
+++ src/components/AppHeader.jsx
@@ -7,7 +7,7 @@ function Header({ onPrint, onErase }) {
     <header className="md:flex md:items-center md:justify-between">
       <div className="min-w-0 flex-1">
         <h2 className="pl-4 text-2xl font-bold leading-7 text-gray-900 sm:truncate sm:text-3xl sm:tracking-tight">
-          CV Creator
+          Swift CV
         </h2>
       </div>
       <div className="mt-4 flex md:ml-4 md:mt-0">
