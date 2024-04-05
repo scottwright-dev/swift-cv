89a1eb5f12f7618a7598301ef2d273f7da010d7c
Scott Wright <108872957+scottwright-dev@users.noreply.github.com>
2024-04-05 18:17:51 +0100

    feat: Add InputSection scrolling
    This commit adds a simple scroll feature to the input form section.
    As the form grows, the user can now scroll to see the rest of the form, while the CV preview remains static.
    Allowing the user to see the Preview in its entirety, providing a better user experience.

--- src/components/SectionContainer.jsx
+++ src/components/SectionContainer.jsx
@@ -41,10 +41,12 @@ export default function SectionContainer() {
       [id]: value,
     }));
   };
+
   // handles creating a pdf / saving a cv
   const handlePrint = useReactToPrint({
     content: () => componentRef.current,
   });
+
   // deletes all user input data in the cv
   const removeAllData = () => {
     localStorage.clear();
@@ -55,13 +57,13 @@ export default function SectionContainer() {
     <>
       <AppHeader onPrint={handlePrint} onErase={removeAllData} />
       <main className="flex min-h-screen flex-col pt-4 md:flex-row">
-        <section className="flex w-full flex-col md:w-2/5">
+        <section className="flex h-screen w-full flex-col overflow-y-auto md:w-2/5">
           <InputSection
             onInputChange={handleInputChange}
             formValues={formValues}
           />
         </section>
-        <section className="flex w-full flex-col md:w-3/5">
+        <section className="flex h-screen w-full flex-col md:w-3/5">
           <div ref={componentRef}>
             <OutputSection formValues={formValues} labelsToIds={labelsToIds} />
           </div>
