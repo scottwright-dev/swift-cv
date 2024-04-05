bb26790242c2a4ac2570333cd8b0feddf81cde89
Scott Wright <108872957+scottwright-dev@users.noreply.github.com>
2024-04-05 19:45:20 +0100

    style: adjust header layout

--- src/components/AppHeader.jsx
+++ src/components/AppHeader.jsx
@@ -1,14 +1,15 @@
 import PropTypes from 'prop-types';
 import ButtonPrimary from './ButtonPrimary';
 import ButtonSecondary from './ButtonSecondary';
+import logo from '../assets/swiftcv-logo.svg';
 
 function Header({ onPrint, onErase }) {
   return (
-    <header className="md:flex md:items-center md:justify-between">
+    <header className="rounded-xl bg-slate-200 p-4 md:flex md:items-center md:justify-between">
       <div className="min-w-0 flex-1">
-        <h2 className="pl-4 text-2xl font-bold leading-7 text-gray-900 sm:truncate sm:text-3xl sm:tracking-tight">
-          Swift CV
-        </h2>
+        <h1>
+          <img src={logo} alt="Swift CV" className="h-auto w-24" />
+        </h1>
       </div>
       <div className="mt-4 flex md:ml-4 md:mt-0">
         <div>
