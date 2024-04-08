e934b46defc3de05a59387df1ca23b041a1b27df
Scott Wright <108872957+scottwright-dev@users.noreply.github.com>
2024-04-08 16:20:39 +0100

    style: Add disclosure panel open/close transition

--- src/components/form/FormSectionDisclosurePanel.jsx
+++ src/components/form/FormSectionDisclosurePanel.jsx
@@ -1,4 +1,4 @@
-import { Disclosure } from '@headlessui/react';
+import { Disclosure, Transition } from '@headlessui/react';
 import { IconChevronRight } from '@tabler/icons-react';
 import PropTypes from 'prop-types';
 import InputFields from './InputFieldGroup';
@@ -24,21 +24,30 @@ function FormSectionDisclosurePanel({
               className={`${open ? 'rotate-90 transform' : ''} h-5 w-5 flex-shrink-0 text-slate-500`}
             />
           </Disclosure.Button>
-          <Disclosure.Panel className="px-4 pb-2 pt-4 text-sm text-gray-500">
-            <InputFields
-              section={section}
-              formValues={formValues}
-              handleFieldChange={handleFieldChange}
-            />
-            {section.addButton && (
-              <div className="mt-4 flex justify-center">
-                <ButtonCircular
-                  text="Add More"
-                  onClick={() => addFieldSet(section.title)}
-                />
-              </div>
-            )}
-          </Disclosure.Panel>
+          <Transition
+            enter="transition duration-100 ease-out"
+            enterFrom="transform scale-95 opacity-0"
+            enterTo="transform scale-100 opacity-100"
+            leave="transition duration-75 ease-out"
+            leaveFrom="transform scale-100 opacity-100"
+            leaveTo="transform scale-95 opacity-0"
+          >
+            <Disclosure.Panel className="px-4 pb-2 pt-4 text-sm text-gray-500">
+              <InputFields
+                section={section}
+                formValues={formValues}
+                handleFieldChange={handleFieldChange}
+              />
+              {section.addButton && (
+                <div className="mt-4 flex justify-center">
+                  <ButtonCircular
+                    text="Add More"
+                    onClick={() => addFieldSet(section.title)}
+                  />
+                </div>
+              )}
+            </Disclosure.Panel>
+          </Transition>
         </>
       )}
     </Disclosure>
