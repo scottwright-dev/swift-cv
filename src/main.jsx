17212bee00478cd22e5805318608cd2b22a0eb53
Scott Wright <108872957+scottwright-dev@users.noreply.github.com>
2024-04-09 15:11:49 +0100

    feat: Add Vercel analytics and dependencies

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,12 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
-import "./index.css";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { inject } from '@vercel/analytics';
+import App from './App.jsx';
+import './index.css';
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+inject();
+
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
