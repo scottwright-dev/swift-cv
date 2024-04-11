efac9bc365efd31c196a18ce82e13d8d1b33a32f
Scott Wright <108872957+scottwright-dev@users.noreply.github.com>
2024-04-11 20:50:37 +0100

    chore: remove redundant footer code

--- src/LandingPage.jsx
+++ src/LandingPage.jsx
@@ -58,7 +58,6 @@ export default function LandingPage({ setIsLandingPage }) {
             </div>
           </div>
         </main>
-
         {/* Footer */}
         <footer aria-labelledby="footer-heading" className="relative">
           <h2 id="footer-heading" className="sr-only">
@@ -66,34 +65,28 @@ export default function LandingPage({ setIsLandingPage }) {
           </h2>
           <div className="mx-auto max-w-7xl px-6 pb-8 pt-4 lg:px-8">
             <div className="border-t border-white/10 pt-8 md:flex md:items-center md:justify-between">
-              <div className="flex space-x-6 md:order-2">
-                {[
-                  {
-                    name: 'GitHub Repository',
-                    href: 'https://github.com/scottwright-dev/swift-cv',
-                    icon: (props) => (
-                      <svg fill="currentColor" viewBox="0 0 24 24" {...props}>
-                        <path
-                          fillRule="evenodd"
-                          d="M12 2C6.477 2 2 6.477 2 12c0 4.418 2.865 8.166 6.839 9.489.5.092.682-.217.682-.482 0-.237-.009-.866-.014-1.7-2.782.602-3.369-1.34-3.369-1.34-.455-1.157-1.11-1.465-1.11-1.465-.908-.62.069-.608.069-.608 1.003.07 1.532 1.03 1.532 1.03.891 1.529 2.341 1.088 2.91.833.091-.646.349-1.086.635-1.337-2.22-.253-4.555-1.11-4.555-4.943 0-1.091.39-1.984 1.03-2.682-.103-.253-.447-1.27.098-2.645 0 0 .84-.27 2.75 1.02A9.564 9.564 0 0112 7.07c.85.004 1.705.115 2.504.337 1.909-1.29 2.747-1.02 2.747-1.02.547 1.375.203 2.392.1 2.645.64.698 1.028 1.59 1.028 2.682 0 3.841-2.337 4.687-4.565 4.936.359.31.678.92.678 1.852 0 1.335-.012 2.415-.012 2.741 0 .267.18.577.688.479C19.137 20.164 22 16.418 22 12c0-5.523-4.477-10-10-10z"
-                        />
-                      </svg>
-                    ),
-                  },
-                ].map((item) => (
-                  <a
-                    key={item.name}
-                    href={item.href}
-                    className="text-blue-200 hover:text-yellow-400"
+              <div className="flex w-full justify-between md:order-2">
+                <p className="text-xs leading-5 text-blue-300 md:mt-0">
+                  &copy; 2024 Swift CV. All rights reserved.
+                </p>
+                <a
+                  href="https://github.com/scottwright-dev/swift-cv"
+                  className="text-blue-200 hover:text-yellow-400"
+                >
+                  <span className="sr-only">GitHub Repository</span>
+                  <svg
+                    fill="currentColor"
+                    viewBox="0 0 24 24"
+                    className="h-6 w-6"
+                    aria-hidden="true"
                   >
-                    <span className="sr-only">{item.name}</span>
-                    <item.icon className="h-6 w-6" aria-hidden="true" />
-                  </a>
-                ))}
+                    <path
+                      fillRule="evenodd"
+                      d="M12 2C6.477 2 2 6.477 2 12c0 4.418 2.865 8.166 6.839 9.489.5.092.682-.217.682-.482 0-.237-.009-.866-.014-1.7-2.782.602-3.369-1.34-3.369-1.34-.455-1.157-1.11-1.465-1.11-1.465-.908-.62.069-.608.069-.608 1.003.07 1.532 1.03 1.532 1.03.891 1.529 2.341 1.088 2.91.833.091-.646.349-1.086.635-1.337-2.22-.253-4.555-1.11-4.555-4.943 0-1.091.39-1.984 1.03-2.682-.103-.253-.447-1.27.098-2.645 0 0 .84-.27 2.75 1.02A9.564 9.564 0 0112 7.07c.85.004 1.705.115 2.504.337 1.909-1.29 2.747-1.02 2.747-1.02.547 1.375.203 2.392.1 2.645.64.698 1.028 1.59 1.028 2.682 0 3.841-2.337 4.687-4.565 4.936.359.31.678.92.678 1.852 0 1.335-.012 2.415-.012 2.741 0 .267.18.577.688.479C19.137 20.164 22 16.418 22 12c0-5.523-4.477-10-10-10z"
+                    />
+                  </svg>
+                </a>
               </div>
-              <p className="mt-8 text-xs leading-5 text-blue-300 md:order-1 md:mt-0">
-                &copy; 2024 Swift CV. All rights reserved.
-              </p>
             </div>
           </div>
         </footer>
