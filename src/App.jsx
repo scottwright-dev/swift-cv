26c76bd7584deeaaebb7ec67f097f35f628c749f
Scott Wright <108872957+scottwright-dev@users.noreply.github.com>
2024-04-11 20:27:24 +0100

    feat: Add landing page and animations to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,29 @@
+import { useState } from 'react';
+import { FadeIn } from './components/Animations';
 import SectionContainer from './components/SectionContainer';
 import AppFooter from './components/AppFooter';
+import LandingPage from './LandingPage';
 
 function App() {
+  const [isLandingPage, setIsLandingPage] = useState(true);
+
   return (
-    <>
-      <div className="mx-auto max-w-7xl px-4 pt-4 sm:px-6 lg:px-8">
-        <SectionContainer />
-        <AppFooter />
-      </div>
-    </>
+    <div
+      className={
+        isLandingPage ? '' : 'mx-auto max-w-7xl px-4 pt-4 sm:px-6 lg:px-8'
+      }
+    >
+      {isLandingPage ? (
+        <LandingPage setIsLandingPage={setIsLandingPage} />
+      ) : (
+        <>
+          <FadeIn>
+            <SectionContainer />
+            <AppFooter />
+          </FadeIn>
+        </>
+      )}
+    </div>
   );
 }
 
