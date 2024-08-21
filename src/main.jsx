2680eeca79bd572574bce19794a34739b3b730f9
Scott Wright <108872957+scottwright-dev@users.noreply.github.com>
2024-08-21 08:30:47 +0100

    chore: Remove vercel speedinsights

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { inject } from '@vercel/analytics';
-import { injectSpeedInsights } from '@vercel/speed-insights';
 import App from './App.jsx';
 import './index.css';
 
 // Vercel Analytics
 inject();
-injectSpeedInsights();
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
